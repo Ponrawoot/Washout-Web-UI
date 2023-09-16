baeb34a9cbc423c84bd7df507da6df62fa2ab5f7
naphatbass <naphatbass@outlook.com>
2023-09-16 18:01:59 +0700

    added Navbar and branchPage
    implemented navbar and branch page
    TODO:
    - adjust style to color and font

--- src/app/branch/page.tsx
+++ src/app/branch/page.tsx
@@ -1,7 +1,69 @@
+import Navbar from "../navbar";
+
 export default function BranchManagement() {
-    return (
-      <div className="text-black">
-        <p>Branch Page</p>
+  return (
+    <main className="bg-white">
+      <Navbar username="Bass" />
+      <div className="container p-10">
+        <h1 className="text-lg">ระบบจัดการสาขา</h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+          เพิ่มสาขา
+        </button>
+        <table className="mt-4 w-full border-collapse border border-gray-300">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border border-gray-300 px-4 py-2">หมายเลขสาขา</th>
+              <th className="border border-gray-300 px-4 py-2 w-3/5">ชื่อสาขา</th>
+              <th className="border border-gray-300 px-4 py-2"></th>
+              <th className="border border-gray-300 px-4 py-2"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border border-gray-300">
+              <td className="border border-gray-300 px-4 py-2 text-center">202001</td>
+              <td className="border border-gray-300 px-4 py-2 text-center">บรรทัดทอง</td>
+              <td className="border border-gray-300 px-4 py-2 text-center">
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded">
+                  แก้ไข
+                </button>
+              </td>
+              <td className="border border-gray-300 px-4 py-2 text-center">
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                  ลบ
+                </button>
+              </td>
+            </tr>
+            <tr className="border border-gray-300">
+              <td className="border border-gray-300 px-4 py-2 text-center">202002</td>
+              <td className="border border-gray-300 px-4 py-2 text-center">สามย่าน</td>
+              <td className="border border-gray-300 px-4 py-2 text-center">
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded">
+                  แก้ไข
+                </button>
+              </td>
+              <td className="border border-gray-300 px-4 py-2 text-center">
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                  ลบ
+                </button>
+              </td>
+            </tr>
+            <tr className="border border-gray-300">
+              <td className="border border-gray-300 px-4 py-2 text-center">202003</td>
+              <td className="border border-gray-300 px-4 py-2 text-center">สยาม</td>
+              <td className="border border-gray-300 px-4 py-2 text-center">
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded">
+                  แก้ไข
+                </button>
+              </td>
+              <td className="border border-gray-300 px-4 py-2 text-center">
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                  ลบ
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    )
-  }
+    </main>
+  );
+}
---
+++ src/app/navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Image from 'next/image';
+import LocalConvenienceStoreIcon from '@mui/icons-material/LocalConvenienceStore';
+import LocalLaundryServiceIcon from '@mui/icons-material/LocalLaundryService';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+
+
+const Navbar = (props) => {
+  const { username } = props; // Destructure the username prop
+
+  return (
+    <nav className="bg-primary-blue p-4">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+            <div className="text-white font-bold text-xl">
+                <LocalConvenienceStoreIcon className='w-10 h-10 mr-4 ml-4 hover:text-gray-600'/>
+                <ManageAccountsIcon className='w-10 h-10 mr-4 ml-4 hover:text-gray-600'/>
+                <LocalLaundryServiceIcon className='w-10 h-10 ml-4 hover:text-gray-600'/>
+            </div>
+          <ul className="flex space-x-4 items-center">
+            <li>
+              <a href="#" className="text-white hover:text-gray-300">{username}</a>
+            </li>
+            <li>
+                {/* <Image src="/public/profileImage.jpg" alt="avatar" width={40} height={40} /> */}
+                <div className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center">
+                    <span className="text-gray-600 text-lg">{username[0]}</span>
+                </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
