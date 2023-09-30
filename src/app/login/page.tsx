db5050ad031e93abd38d85f6873b802a5a47965f
Ponrawoot <minnaja853@gmail.com>
2023-09-30 18:47:36 +0700

    add unique username and password

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,61 @@
-export default function LoginPage() {
+"use client"
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import styles from './page.module.css';
+import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
+import Button from '@mui/material/Button';
+
+export default function Home() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter()
+
+  const handleLogin = () => {
+    if (username === 'Admin' && password === '1234') {
+      router.push('/branch');
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
   return (
-    <div>
-      <p>Login Page</p>
-    </div>
-  )
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <p>Login</p>
+        </div>
+
+        <div className={styles.inputs}>
+          <div className={styles.input}>
+            <PersonIcon className={styles.icon} />
+            <input
+              type='text'
+              placeholder='Username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className={styles.inputs}>
+          <div className={styles.input}>
+            <LockIcon className={styles.icon} />
+            <input
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className={styles.submit_container}>
+          <Button className={styles.submit} variant="contained" onClick={handleLogin}>
+            Log In
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
