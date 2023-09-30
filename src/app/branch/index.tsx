0ac099228d28e8ece6ef002aa63dd7e3d3795cf1
reine298 <kanjanee10704@gmail.com>
2023-09-30 15:14:10 +0700

    Jn : LogIn page

---
+++ src/app/branch/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import '../branch/LogIn.css';
+import LogIN from './page';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <LogIN />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import styles from './page.module.css';
+import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
+import Button from '@mui/material/Button';
+
 export default function Home() {
   return (
-    <main className="text-black">
-      <p>Login Page</p>
+    <main className={styles.main}>
+
+      <div className={styles.container}>
+        <div className={styles.header}>
+            <p>Login</p>
+        </div>
+        
+        <div className={styles.inputs}>
+            <div className={styles.input}>
+              < PersonIcon className={styles.icon} />
+              <input type='text' placeholder='Username'/>
+            </div>
+        </div>
+
+        <div className={styles.inputs}>
+            <div className={styles.input}>
+              < LockIcon className={styles.icon} />
+                <input type='password' placeholder='Password'/>
+            </div>
+        </div>
+        
+        <div className={styles.submit_container}>
+            <Button className={styles.submit} variant="contained">Log In</Button>
+        </div>
+        
+        </div>
+
     </main>
   )
 }
