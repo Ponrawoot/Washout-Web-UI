4559ce3dec9f6a63ae5ee1f1d1b4320458b6d6ee
Ponrawoot <minnaja853@gmail.com>
2023-09-30 18:48:56 +0700

    add automatic routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,15 @@
-import styles from './page.module.css';
-import PersonIcon from '@mui/icons-material/Person';
-import LockIcon from '@mui/icons-material/Lock';
-import Button from '@mui/material/Button';
+"use client"
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
+const Page = () => {
+  const router = useRouter();
 
-      <div className={styles.container}>
-        <div className={styles.header}>
-            <p>Login</p>
-        </div>
-        
-        <div className={styles.inputs}>
-            <div className={styles.input}>
-              < PersonIcon className={styles.icon} />
-              <input type='text' placeholder='Username'/>
-            </div>
-        </div>
+  useEffect(() => {
+    router.push('/login');
+  }, [router]);
 
-        <div className={styles.inputs}>
-            <div className={styles.input}>
-              < LockIcon className={styles.icon} />
-                <input type='password' placeholder='Password'/>
-            </div>
-        </div>
-        
-        <div className={styles.submit_container}>
-            <Button className={styles.submit} variant="contained">Log In</Button>
-        </div>
-        
-        </div>
+  return null;
+};
 
-    </main>
-  )
-}
+export default Page;
