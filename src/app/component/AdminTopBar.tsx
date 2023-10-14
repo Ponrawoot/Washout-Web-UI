88fea7922e054355098993d1eafa9009deb43c7d
Ponrawoot <minnaja853@gmail.com>
2023-10-14 18:20:08 +0700

    make the icon response to the path

--- src/app/component/AdminTopBar.tsx
+++ src/app/component/AdminTopBar.tsx
@@ -3,34 +3,51 @@ import { useRouter } from "next/navigation";
 import LocalConvenienceStoreIcon from "@mui/icons-material/LocalConvenienceStore";
 import LocalLaundryServiceIcon from "@mui/icons-material/LocalLaundryService";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import { useState, useEffect } from "react";
 
 export default function AdminTopBar() {
   const router = useRouter();
+  const [isBranchPage, setIsBranchPage] = useState(false)
+  const [isStaffPage, setIsStaffPage] = useState(false)
+  const [isMachinePage, setIsMachinePage] = useState(false)
+
+  const handleBranchIconClick = () => {
+    setIsBranchPage(true);
+    setIsStaffPage(false);
+    setIsMachinePage(false);
+    router.replace("/branch");
+  };
+
+  const handleStaffIconClick = () => {
+    setIsStaffPage(true);
+    setIsBranchPage(false);
+    setIsMachinePage(false);
+    router.replace("/staff");
+  };
+
+  const handleMachineIconClick = () => {
+    setIsMachinePage(true);
+    setIsStaffPage(false);
+    setIsBranchPage(false);
+    router.replace("/machine");
+  };
+
   return (
     <div className="bg-primary-blue p-4">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <div className="text-white font-bold text-xl">
             <LocalConvenienceStoreIcon
-              className="w-10 h-10 mr-4 ml-4 hover:text-gray-600"
-              onClick={(e) => {
-                e.stopPropagation();
-                router.replace("/branch");
-              }}
+              className={`w-10 h-10 mr-4 ml-4 hover:text-gray-600 ${isBranchPage ? 'text-gray-600' : ''}`}
+              onClick={handleBranchIconClick}
             />
             <ManageAccountsIcon
-              className="w-10 h-10 mr-4 ml-4 hover:text-gray-600"
-              onClick={(e) => {
-                e.stopPropagation();
-                router.replace("/staff");
-              }}
+              className={`w-10 h-10 mr-4 ml-4 hover:text-gray-600 ${isStaffPage ? 'text-gray-600' : ''}`}
+              onClick={handleStaffIconClick}
             />
             <LocalLaundryServiceIcon
-              className="w-10 h-10 ml-4 hover:text-gray-600"
-              onClick={(e) => {
-                e.stopPropagation();
-                router.replace("/machine");
-              }}
+              className={`w-10 h-10 mr-4 ml-4 hover:text-gray-600 ${isMachinePage ? 'text-gray-600' : ''}`}
+              onClick={handleMachineIconClick}
             />
           </div>
           <ul className="flex space-x-4 items-center">
